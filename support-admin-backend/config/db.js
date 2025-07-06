228350e52360ba9bb34b061a5de6b91efa3f97b2
Pratiksha Bhise <pratikshabhise87@gmail.com>
2025-07-06 16:44:00 +0530

    first commit

--- support-admin-backend/config/db.js
+++ support-admin-backend/config/db.js
@@ -1,16 +1,20 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false, // important for Render!
+  },
 });
 
-pool.connect()
-    .then(() => console.log('Connected to PostgreSQL'))
-    .catch(err => console.error('Database connection error', err));
+pool
+  .connect()
+  .then(() => console.log("Connected to PostgreSQL"))
+  .catch((err) => console.error("Database connection error", err));
 
 module.exports = pool;
